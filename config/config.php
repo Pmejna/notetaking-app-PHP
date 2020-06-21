8e27dd642e45c773b1d1e0d79732cccbb4411813
Przemyslaw Mejna <pmejna@gmail.com>
2020-06-21 23:17:52 +0100

    create config.php, create database configuration

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php 
+
+declare(strict_types=1);
+
+$configuration = [
+    'host' => 'localhost',
+    'database' => 'notes',
+    'user' => 'user_notes',
+    'password' => 'ZJfGrQqqjRBvVDnA',
+];
